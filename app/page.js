51afa37642e5969aa8be37a8b4a23e8d51ea9bf2
khantact <kevinhan0@gmail.com>
2024-03-22 17:34:06 -0400

    fixed highlighting and added preloaded code

--- app/page.js
+++ app/page.js
@@ -1,22 +1,18 @@
 "use client";
-import Ide from "@/components/ide";
+import Ide from "@/components/Ide";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const code = `def main():
-  print("test")
-  `;
   const [codeData, setData] = useState();
-  const [initialCode, setCode] = useState(code);
 
   return (
     <main>
       <div className="columns-2 m-12">
         <div className="border-black border-4 rounded-md">
-          <Ide dataState={setData} initialState={initialCode} />
+          <Ide />
         </div>
         <div className="bg-black text-white p-6 rounded-md">
-          <p>This is where the suggestions should go</p>
+          <p>What is this code missing?</p>
         </div>
       </div>
     </main>
--- components/Ide.js
+++ components/Ide.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AceEditor from "react-ace";
+import { useState } from "react";
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/theme-clouds_midnight";
 import "ace-builds/src-noconflict/ext-language_tools";
-export default function Ide({ dataState, initialState }) {
-  const markers = [
+export default function Ide() {
+  const [code, setCode] = useState("");
+
+  useEffect(() => {
+    setCode(`
+    s = ''.join(char.lower() for char in s if char.isalnum())
+    return s == s[::-1]
+
+test_string = "A man, a plan, a canal, Panama!"
+print(is_palindrome(test_string))`);
+  }, []);
+
+  const handleChange = (newCode) => {
+    setCode(newCode);
+  };
+
+  const marker = [
     {
-      startRow: 1,
-      startCol: 0,
-      endRow: 4,
-      endCol: Number.MAX_SAFE_INTEGER, // Set endCol to a very large value to span entire row
-      className: "bg-yellow-200", // Using Tailwind CSS classes
+      startRow: 0,
+      endRow: 1,
+      className: "bg-yellow-200 absolute",
       type: "background",
     },
   ];
+
   return (
     <div>
       <AceEditor
         mode="python"
         placeholder="Write Code!"
-        onChange={dataState}
-        markers={markers}
+        onChange={handleChange}
         width="100%"
-        value={initialState}
+        value={code}
+        markers={marker}
         fontSize={24}
       ></AceEditor>
     </div>
